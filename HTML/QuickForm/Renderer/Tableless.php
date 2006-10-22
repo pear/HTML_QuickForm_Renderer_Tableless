35803377181ff188bed73c363a0bc55d26f97202
Mark Wiesemann <wiesemann@php.net>
2006-10-22 11:39:47 +0000

    - switch to New BSD License, update changelog accordingly
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_QuickForm_Renderer_Tableless/trunk@222224 c90b9560-bf6c-de11-be94-00142212c4b1

--- HTML/QuickForm/Renderer/Tableless.php
+++ HTML/QuickForm/Renderer/Tableless.php
@@ -5,11 +5,13 @@
  *
  * PHP versions 4 and 5
  *
- * LICENSE: This source file is subject to version 3.01 of the PHP license
- * that is available through the world-wide-web at the following URI:
- * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
- * the PHP License and are unable to obtain it through the web, please
- * send a note to license@php.net so we can mail you a copy immediately.
+ * LICENSE: This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.opensource.org/licenses/bsd-license.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to wiesemann@php.net so we can send you a copy immediately.
  *
  * @category   HTML
  * @package    HTML_QuickForm_Renderer_Tableless
@@ -18,7 +20,7 @@
  * @author     Bertrand Mansion <bmansion@mamasam.com>
  * @author     Mark Wiesemann <wiesemann@php.net>
  * @copyright  2005-2006 The PHP Group
- * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @license    http://www.opensource.org/licenses/bsd-license.php New BSD License
  * @version    CVS: $Id$
  * @link       http://pear.php.net/package/HTML_QuickForm_Renderer_Tableless
  */
@@ -38,7 +40,7 @@ require_once 'HTML/QuickForm/Renderer/Default.php';
  * @author     Adam Daniel <adaniel1@eesus.jnj.com>
  * @author     Bertrand Mansion <bmansion@mamasam.com>
  * @author     Mark Wiesemann <wiesemann@php.net>
- * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @license    http://www.opensource.org/licenses/bsd-license.php New BSD License
  * @version    Release: @package_version@
  * @link       http://pear.php.net/package/HTML_QuickForm_Renderer_Tableless
  */
